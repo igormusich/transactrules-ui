5bc2163171e80261d75ba9c4b3148cb170bd1398
igormusic <igormusich@gmail.com>
2018-01-06 18:37:41 -0500

    work in progress - no errors

--- src/app/pages/settings/account-type/account-type.module.ts
+++ src/app/pages/settings/account-type/account-type.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { BreadcrumbsModule } from '../../../core/breadcrumbs/breadcrumbs.module';
+import { CdkTableModule } from '@angular/cdk/table';
+import { ScrollbarModule } from '../../../core/scrollbar/scrollbar.module';
+import { PageHeaderModule } from '../../../core/page-header/page-header.module';
+
 import { AccountTypeRoutingModule } from './account-type-routing.module';
 import { AccountTypeComponent } from 'app/pages/settings/account-type/account-type.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    AccountTypeRoutingModule
+    AccountTypeRoutingModule,
+    FormsModule,
+    FlexLayoutModule,
+    ScrollbarModule,
+    PageHeaderModule,
+    BreadcrumbsModule,
+    MatTableModule,
+    CdkTableModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ],
   declarations: [AccountTypeComponent],
   exports: [
